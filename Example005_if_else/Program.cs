f4f4a7fc01b699fe581247b3aaa6e7d78308bcf9
modzhin <modzhds@gmail.com>
2022-12-16 00:49:50 +0300

    Example005_if_else

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя: ");
+string name = Console.ReadLine();
+if (name.ToLower() == "дима")
+{
+    Console.WriteLine("Ура, это " + name + "!");
+}
+else
+{
+    Console.WriteLine("Привет, " + name);
+}
+
